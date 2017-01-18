5a3b5b30dec32fa06fcc33ebc26f840814cc39d8
Fernando Dantas <fernandojrsud@hotmail.com>
2017-01-18 11:11:39 -0300

    Criando build de javascript com node dentro do sublime text

--- teste.js
+++
@@ -1,9 +0,0 @@
-var a = 15;
-var b = 5;
- 
-var resultado = a + b;
-
-console.log('Resultado:', resultado);
-
-//Para criar um build no ublime vc tem que passar o caminho do node intalado na sua maquina nesse caso foi o C:nodejs o script esta no arquivo node.sublime.build. 
-//No sublime para criar um novo build vai em tools->Build System-> new build System criar e salvaque ele ja salva debtro da pasta do sublime.
