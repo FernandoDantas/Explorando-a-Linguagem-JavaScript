83e09c20db88067e1e7df72dc8ace7f532a74a54
Fernando Dantas <fernandojrsud@hotmail.com>
2017-01-18 11:31:37 -0300

    Aprendendo sobre variaveis e hoistings

---
+++ variaveis-e-hoisting/variaveis_e_hoisting.js
@@ -0,0 +1,18 @@
+
+
+var percentualImposto = 0.15;
+valor = 100;
+var totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar', totalImposto);
+
+var valor;
+
+//HOISTING -> lEVA PRA CIMA TODAS AS DECLARAÇÕES DAS VARIÁVEIS QUE ELE ENCONTAR MAIS NÃO AS ATRIBUIÇÕES
+/*
+var percentualImposto, valor, totalImposto;
+percentualImposto = 0.15;
+valor = 100;
+totalImposto = valor * percentualImposto;
+console.log('Total imposto a pagar', totalImposto);
+*/
